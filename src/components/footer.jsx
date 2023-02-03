b360bfdfb0606b3f0bebab6d171556c7b313ab44
aman-mishra-07 <aman7986355740@gmail.com>
2023-02-03 11:52:03 +0530

    First project added

---
+++ src/components/footer.jsx
@@ -0,0 +1,4 @@
+export default function Footer()
+{
+    return <h2>This is footer </h2>
+}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import './navbar.css'
+export default function Navbar(){
+    return (
+        <div className="navbar">
+            <ul>
+                <li><a href=" ">Home    </a></li>
+                <li><a href=" ">Services</a></li>
+                <li><a href=" ">Contact </a></li>
+                <li><a href=" ">About Us</a></li>
+            </ul>
+        </div>
+    )
+}
